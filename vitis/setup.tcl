28bd68d421e03944797cb224158be719145a0340
pete <peter.dudley@precisionplanting.com>
2022-08-11 14:05:33 -0400

    hello1 app tests fpga register access and runs in ddr memory

--- vitis/setup.tcl
+++ vitis/setup.tcl
@@ -15,14 +15,14 @@ setws ./workspace
 platform create -name "standalone_plat" -hw $hw -proc $proc -os standalone
 #platform create -name "freertos_plat" -hw $hw -proc $proc -os freertos
 
-#app create -name hello1 -platform standalone_plat -domain standalone_domain -template "Empty Application(C)"
-#file link -symbolic ./workspace/hello1/src/hello1.c             ../../../src/hello1/hello1.c
-#file link -symbolic ./workspace/hello1/src/fpga.h               ../../../src/fpga.h
-#file link -symbolic ./workspace/hello1/src/platform.c           ../../../src/hello1/platform.c
-#file link -symbolic ./workspace/hello1/src/platform.h           ../../../src/hello1/platform.h
-#file link -symbolic ./workspace/hello1/src/platform_config.h    ../../../src/hello1/platform_config.h
-#file delete -force  ./workspace/hello1/src/lscript.ld
-#file link -symbolic ./workspace/hello1/src/lscript.ld           ../../../src/hello1/lscript.ld
+app create -name hello1 -platform standalone_plat -domain standalone_domain -template "Empty Application(C)"
+file link -symbolic ./workspace/hello1/src/hello1.c             ../../../src/hello1/hello1.c
+file link -symbolic ./workspace/hello1/src/fpga.h               ../../../src/fpga.h
+file link -symbolic ./workspace/hello1/src/platform.c           ../../../src/hello1/platform.c
+file link -symbolic ./workspace/hello1/src/platform.h           ../../../src/hello1/platform.h
+file link -symbolic ./workspace/hello1/src/platform_config.h    ../../../src/hello1/platform_config.h
+file delete -force  ./workspace/hello1/src/lscript.ld
+file link -symbolic ./workspace/hello1/src/lscript.ld           ../../../src/hello1/lscript.ld
 
 
 
