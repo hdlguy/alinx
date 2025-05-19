f3c3f8c1b48c1b73df8b0674c2750aa44d234d05
Your Name <you@example.com>
2025-05-19 19:59:13 +0000

    working on C apps for instrument application

--- AXU2CG_full/apps/bram_test/test.c
+++ AXU2CG_full/apps/bram_test/test.c
@@ -59,32 +59,3 @@ int main(int argc,char** argv)
 }
 
 
-/*
-void* phy_addr_2_vir_addr(off_t phy_addr,size_t size)
-{
-   void* vir_addr=NULL;
-
-   int fd = open("/dev/mem",O_RDWR|O_SYNC);
-   if(fd < 0)
-   {
-       fprintf(stderr,"Can't open /dev/mem\n");
-   }
-   else
-   {
-                  //0 is not NULL
-      vir_addr=mmap(0,size,PROT_READ|PROT_WRITE,MAP_SHARED,fd,phy_addr);
-      if(vir_addr == NULL)
-      {
-          fprintf(stderr,"Can't mmap\n");
-      }
-      else
-      {
-          //DEBUG("phy_addr 0x%lX mapped to 0x%lX with size=0x%x bytes\n",phy_addr,(uint64_t)vir_addr,(uint32_t)size);
-          //DEBUG("phy_addr 0x%lx mapped to 0x%p with size=0x%zx bytes\n", phy_addr, vir_addr, size);
-      }
-   }
-   return vir_addr;
-}
-
-
-*/
