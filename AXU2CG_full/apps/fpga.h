171e87399e8d9301bcc4f436925093afe998fd43
Your Name <you@example.com>
2025-05-22 15:29:20 +0000

    run_vinstru.sh work

--- AXU2CG_full/apps/fpga.h
+++ AXU2CG_full/apps/fpga.h
@@ -9,18 +9,19 @@
 #define     FPGA_VERSION            1
 #define     FPGA_LED                2 // [0] = PL LED value, read/write
 
-#define     VINSTRU_PULSE_ENABLE    3 // [0] = pulse enable (rw)
-#define     VINSTRU_CAPTURE_CONTROL 4 // [0] = capture run (rw), [4] = capture done (ro), [8] = reset (rw)
+#define     VINSTRU_PULSE_ENABLE    3 // [0] = pulse enable (rw), starts the pulse generator
+#define     VINSTRU_CAPTURE_CONTROL 4 // [0] = capture run (rw), [4] = capture done (ro), [8] = reset (rw), 
 #define     VINSTRU_PULSE_PERIOD    5 // [15:0] = period between pulses in samples (rw)
 #define     VINSTRU_PULSE_WIDTH     6 // [15:0] = pulse width in samples (rw)
 #define     VINSTRU_PULSE_AMPLITUDE 7 // [15:0] = pulse amplitide in counts (rw)
 #define     VINSTRU_NOISE_AMPLITUDE 8 // [15:0] = noise amplitude (standard deviation) in counts (rw)
 
 
-
+// Test Bram (rw)
 #define     TEST_RAM_OFFSET         0x00010000
 #define     TEST_RAM_SIZE           0x00001000 // 4k
 
+// Vinstru Bram that gets the capture output (rw)
 #define     VINSTRU_BRAM_OFFSET      0x00040000
 #define     VINSTRU_BRAM_SIZE        0x00004000 // 16k
 
--- AXU2CG_full/apps/vinstru/run_vintstru.sh
+++ AXU2CG_full/apps/vinstru/run_vintstru.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-./set_fpga_led 1
+./set_fpga_led 1 # turn on the PL LED
+./get_fpga_version;
 ./set_pulse_params 10000 800 +30000 3000 # period, width, pulse amplitude, noise amplitude
 ./enable_pulse
 ./capture_waveform
 ./read_vinstru_bram 2000 > junk.txt
-./set_fpga_led 0
+./set_fpga_led 0 # turn off the PL LED
 
 
 
