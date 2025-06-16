fffbf675e9b731c57e72b762dfd70474e4ec9ab0
hdlguy <hdlguy@hdlguy.com>
2025-06-16 18:34:47 -0400

    updated vitis stuff to run hello1 with mem_regfile

--- AXU2CG_full/vitis/src/hello1/hello1.c
+++ AXU2CG_full/vitis/src/hello1/hello1.c
@@ -12,16 +12,19 @@ int main()
 
     int unused;
 
-    uint32_t *regptr = (uint32_t *)XPAR_M00_AXI_BASEADDR;
+    uint32_t *regptr = (uint32_t *)XPAR_REGFILE_CTRL_S_AXI_BASEADDR;
     xil_printf("FPGA_VERSION = 0x%08x, FPGA_ID = 0x%08x\r\n", regptr[FPGA_VERSION], regptr[FPGA_ID]);
 
 
     int count = 0;
     int acc = 0;
     for(;;){
-        for(int i=0; i<10000000; i++) acc += count;
+    	
         xil_printf("0x%08x, 0x%08x: Hello!\r\n", count, acc);
+        
+        for(int i=0; i<10000000; i++) acc += count;
         count++;
+        
     }
 
     cleanup_platform();
