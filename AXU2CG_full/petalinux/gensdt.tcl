1d62f19a693dc1e01862543f634b705138eb7de8
hdlguy <hdlguy@hdlguy.com>
2026-08-14 15:24:20 -0400

    EDF with 2026.1 tools.

--- AXU2CG_full/petalinux/gensdt.tcl
+++
@@ -1,5 +0,0 @@
-sdtgen set_dt_param -dir ./sdt
-sdtgen set_dt_param -xsa ../implement/results/top.xsa                                    
-sdtgen set_dt_param -include_dts ./system-user.dtsi
-sdtgen generate_sdt      
-
